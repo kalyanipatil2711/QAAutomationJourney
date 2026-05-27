aed568afb914aabb27944599702363d2860364e6
kalyanipatil2711 <patilkalyani691@gmail.com>
2026-05-27 23:45:14 +0530

    swapping two numbers program

---
+++ src/Swapping2Numbers.java
@@ -0,0 +1,29 @@
+
+public class Swapping2Numbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//		int a =5;
+//		int b=10;
+//		int temp;
+//		temp=a;
+//		a=b;
+//		b=temp;
+//		
+//		System.out.println(a);
+//		System.out.println(b);
+//		System.out.println("swapping no :-"+ a+" " + b);
+		//another method 
+		int e=10;
+		int f=20;
+		
+		e= e+f;
+		f=e-f;
+		e=e-f;
+		System.out.println(e);
+		System.out.println(f);
+		
+	}
+
+}
