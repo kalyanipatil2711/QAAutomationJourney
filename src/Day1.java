d4de8f985a826580d96f38c796eadb94d3b1720e
kalyanipatil2711 <patilkalyani691@gmail.com>
2026-05-27 17:42:09 +0530

    Day 1 - first Java program

---
+++ src/Day1.java
@@ -0,0 +1,12 @@
+
+public class Day1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Day 1 - My QA Automation journey starts today!");
+        System.out.println("I am: Kalyani Patil");
+        System.out.println("Goal: Switch from Manual to Automation QA");
+
+	}
+
+}
