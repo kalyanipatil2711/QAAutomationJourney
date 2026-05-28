f119f5eda88482746f92d21e76c56328200bb95c
kalyanipatil2711 <patilkalyani691@gmail.com>
2026-05-28 15:17:47 +0530

    Day 2 - variables, operators, swap, simple interest, rectangle

---
+++ src/Day2.java
@@ -0,0 +1,44 @@
+
+public class Day2 {
+
+	public static void main(String[] args) {
+	//	 TODO Auto-generated method stub
+		String name = "kalyani";
+		int age= 29;
+		double salary = 90.00;
+		boolean isActive= true;
+		
+		System.out.println("name?"+ name );
+		System.out.println("age ?"+ age);
+		System.out.println("salary"+ salary);
+		System.out.println("Active"+isActive);
+		//arithmatic 
+		int a=10;int b=3;
+		System.out.println("sum"+ (a+b));
+		System.out.println("remainder"+ a%b);
+		//simple intrest
+		int p=100;
+		int r=5;
+		int t=2;
+		
+		double si =( p*r*t)/100.0;
+		System.out.println("principal "+ p);
+		System.out.println("rate of intrest "+ r );
+		System.out.println("time "+ t);
+		System.out.println("simple intrest "+ si);
+       // Area and Perimeter of Rectangle
+		
+		int lenght = 30;
+		int width=20;
+		
+		int area = lenght*width;
+		int perimeter = 2*(lenght+width);
+		 System.out.println("area of rectangle"+area);
+		 System.out.println("Perimeter of Rectangle" + perimeter);
+		
+		
+		
+		
+	}
+
+}
